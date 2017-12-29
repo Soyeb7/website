0c4fe7fffee3447f0eb9d7f8f8b49de122bef6ac
Soyeb7 <soyeb277@gmail.com>
2017-12-29 21:22:28 +0000

    Final website done
    I havent done as good as I would like to have done.

--- javascript/language_changer.js
+++
@@ -1,4 +0,0 @@
-/*
-https://stackoverflow.com/questions/32008125/using-javascript-to-change-website-language
-https://codepen.io/codepajamas/pen/NjGOMV
-*/
--- javascript/weather.js
+++
@@ -1,9 +0,0 @@
-<script src='//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/d3.min.js'></script>
-<script>
-window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];
-window.myWidgetParam.push({id: 11,cityid: '2643743',appid: '5e4ed53c828039fe07c9ecfe5586fda0',
-units: 'metric',containerid: 'openweathermap-widget-11',  });
-(function() {var script = document.createElement('script');
-script.async = true;script.charset = "utf-8";
-script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(script, s);  })();
-</script>
